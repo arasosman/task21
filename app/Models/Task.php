dc6a14f5f54dd2c15afe36e52c31974be01fd0a4
Osman Aras <osmanaras50@gmail.com>
2022-03-19 16:26:56 +0300

    Fix Unit test for Task Model --- add assign relation and SortByStatus scope

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,7 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Task
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property int $user_id
+ * @property int $status
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ *
+ * @property User $assignedUser
+ */
 class Task extends Model
 {
     use HasFactory;
@@ -14,4 +29,24 @@ class Task extends Model
     const DONE = 'DONE';
 
     protected $fillable = ['title', 'description', 'status', 'user_id'];
+
+    public function assignedUser(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function scopeSortByStatus()
+    {
+        $sortPriority = [
+            self::DOING => 1,
+            self::TODO => 2,
+            self::DONE => 3,
+        ];
+
+        return Task::all()
+            ->sortBy('title')
+            ->sortBy(function ($task) use ($sortPriority) {
+                return $sortPriority[$task->status];
+            });
+    }
 }
--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -12,13 +12,13 @@ class TaskFactory extends Factory
      *
      * @return array
      */
-    public function definition()
+    public function definition(): array
     {
         return [
             'title' => $this->faker->sentence,
             'description' => $this->faker->paragraph,
             'status' => $this->faker->randomElement([Task::TODO, Task::DONE, Task::DOING]),
-            'user_id' => $this->faker->numberBetween(1, 10),
+            'user_id' => UserFactory::new()->create()->id,
         ];
     }
 }
